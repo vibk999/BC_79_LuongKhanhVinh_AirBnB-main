1ac39eb473fe3b22cc35a1f74b67b8a7997af98d
vibk999 <vibk9999@gmail.com>
2025-05-17 15:05:24 +0700

    final update

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://airbnbnew.cybersoft.edu.vn/api";
 export const TOKEN_CYBERSOFT =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOT0RFSlMgNTAiLCJIZXRIYW5TdHJpbmciOiIwMi8xMC8yMDI1IiwiSGV0SGFuVGltZSI6IjE3NTkzNjMyMDAwMDAiLCJuYmYiOjE3NDA4NDg0MDAsImV4cCI6MTc1OTUxMDgwMH0.f98XSLVNOxLSj8VvAmW5aDwnFeCxgcK_cCeBu6jlVkU";
-
+//test
 // "id": 46984,
 //       "name": "Vinh",
 //       "email": "vibk9999@gmail.com",
@@ -12,13 +12,22 @@ export const TOKEN_CYBERSOFT =
 //       "gender": true,
 //       "role": "USER"
 // {
-//   "id": 43602,
-//   "name": "admin nè",
-//   "email": "1234@gmail.com",
-//   "password": "123456789",
-//   "phone": "1234567891",
-//   "birthday": "2025-04-30",
-//   "avatar": "",
-//   "gender": true,
-//   "role": "ADMIN"
+//      "id": 44715,
+// "name": "nhan21",
+// "email": "nhan@gmail.com",
+// "password": "111",
+// "phone": "0937123123",
+// "birthday": "2024-01-12",
+// "avatar": "",
+// "gender": true,
+// "role": "ADMIN"
 // },
+//  "id": 44720,
+//       "name": "thanh2",
+//       "email": "thanhdanh123@gmail.com",
+//       "password": "1234",
+//       "phone": "0966048870",
+//       "birthday": "29-12-2004",
+//       "avatar": "",
+//       "gender": true,
+//       "role": "ADMIN"
